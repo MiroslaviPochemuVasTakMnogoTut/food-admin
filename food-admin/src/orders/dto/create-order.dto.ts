a10b53bd4942538bb9000144045861d3eadc87f7
Miroslav <miroslav-pankratov@mail.ru>
2023-12-07 11:26:31 +0700

    create order

--- food-admin/src/orders/dto/create-order.dto.ts
+++ food-admin/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,4 @@
-export class CreateOrderDto {}
+import { OmitType } from '@nestjs/swagger';
+import { Order } from '../entities/order.entity';
+
+export class CreateOrderDto extends OmitType(Order, ['id', 'created_at', 'completed_at']) {}
